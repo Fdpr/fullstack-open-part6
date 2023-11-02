30b1cbf2050b42cbe7d5b062bf3647f6649ded34
Florian <70607158+Fdpr@users.noreply.github.com>
2023-11-02 17:02:26 +0200

    6.9

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,16 +1,15 @@
-
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
-
+import Filter from './components/Filter'
 
 const App = () => {
-  
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      <AnecdoteList/>     
-      <AnecdoteForm/>
+      <Filter />
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
--- anecdotes/src/components/AnecdoteList.jsx
+++ anecdotes/src/components/AnecdoteList.jsx
@@ -3,10 +3,10 @@ import { useSelector, useDispatch } from 'react-redux'
 import Anecdote from "./Anecdote"
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const state = useSelector(state => state)
     const dispatch = useDispatch()
 
-    return anecdotes.toSorted((a, b) => b.votes - a.votes).map(anecdote =>
+    return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase())).toSorted((a, b) => b.votes - a.votes).map(anecdote =>
         <Anecdote key={anecdote.id} anecdote={anecdote} handleVote={() => dispatch(vote(anecdote.id))} />)
 
 }
